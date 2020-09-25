3224f69cf8b48d585bcf225fd040236eb0541972
director <director@gmail.com>
2020-09-25 13:33:58 +0200

    version4

--- src/Welcome.java
+++ src/Welcome.java
@@ -2,11 +2,7 @@
 public class Welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		int var;
-		
-		var=0;
 		
 		System.out.println(" Welcome to Java Programing ");
 	}
